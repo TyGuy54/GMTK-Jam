95de1c6e71d2c96c7af598a96743e4e0e297a35b
TyGuy54 <tylerross107@gmail.com>
2023-07-08 15:58:46 -0400

    fixing necro shooting

--- Scenes/Nector/projectile.gd
+++ Scenes/Nector/projectile.gd
@@ -5,7 +5,8 @@ var speed = 750
 func _physics_process(delta):
 	position += transform.x * speed * delta
 
-func _on_Bullet_body_entered(body):
-	if body.is_in_group("replace_with_zanes_group"):
+
+func _on_projectile_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
 		body.queue_free()
 	queue_free()
--- Scripts/Nector/nector.gd
+++ Scripts/Nector/nector.gd
@@ -2,20 +2,21 @@ extends Actor
 
 @export var Bullet : PackedScene
 
-func _physics_process(delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	spot_nearest_enemy()
 	
 func spot_nearest_enemy():
 	# loops over the groups array
-	for node in get_tree().get_nodes_in_group("replace_with_zanes_group"):
+	for node in get_tree().get_nodes_in_group("player"):
 		# finds the nearest position of something in that group relitive to the necromancer
-		if node.position == self.position:
+		if node.position >= self.position:
 			# if it is then the necromancer shoots
 			shoot()
 			
 func shoot():
+	print("shoot")
 	# instanciantes the bullet sceane
 	var b = Bullet.instantiate()
 	# adds the node as a child 
 	add_child(b)
-#	b.transform = $Marker2D.global_transform
+	b.transform = $Marker2D.global_transform
