a12caaea6f17000838fde9b1079d888d1728a330
TyGuy54 <tylerross107@gmail.com>
2023-07-08 16:47:31 -0400

    projectiles rotate

--- Scenes/Nector/projectile.gd
+++ Scenes/Nector/projectile.gd
@@ -1,12 +1,19 @@
 extends Area2D
 
 var speed = 750
+var rotation_speed = 2.0
+var vel = Vector2.ZERO
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
-
-
+	
+	for node in get_tree().get_nodes_in_group("player"):
+		var direction = (node.global_position - global_position).normalized()
+		var angle_to = $Sprite2D.transform.x.angle_to(direction)
+		$Sprite2D.rotate(sign(angle_to) * min(delta * rotation_speed, abs(angle_to)))
+	
 func _on_projectile_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.queue_free()
+		
 	queue_free()
--- Scripts/Nector/nector.gd
+++ Scripts/Nector/nector.gd
@@ -1,9 +1,18 @@
 extends Actor
 
+@export var cooldown = 0.50
 @export var Bullet : PackedScene
+
+var can_shoot = true
+
+func _ready():
+	start()
 
 func _process(delta: float) -> void:
 	spot_nearest_enemy()
+	
+func start():
+	$Projectile_Cooldown.wait_time = cooldown
 	
 func spot_nearest_enemy():
 	# loops over the groups array
@@ -12,12 +21,18 @@ func spot_nearest_enemy():
 		if node.position >= self.position:
 			# if it is then the necromancer shoots
 			shoot()
-			
+
 func shoot():
-	print("shoot")
+	if not can_shoot:
+		return
+	can_shoot = false
+	$Projectile_Cooldown.start()
+	
 	# instanciantes the bullet sceane
 	var b = Bullet.instantiate()
 	# adds the node as a child 
-	add_child(b)
+	owner.add_child(b)
 	b.transform = $Marker2D.global_transform
 	
+func _on_projectile_cooldown_timeout() -> void:
+	can_shoot = true
