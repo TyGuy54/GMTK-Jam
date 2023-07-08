8aed69628a19ff0c36e8597bad18f2d4b61aef6d
zanecosmo <zanecosmo@gmail.com>
2023-07-08 15:54:11 -0400

    spawner stuff

--- Scripts/spawner.gd
+++ Scripts/spawner.gd
@@ -1,23 +1,36 @@
 extends Node2D
 
+
 @export var spawn_radius: float
 @export var spawn_interval: float
 
-var spawn_timer: Timer
-var radius: Vector2
-	
-func _ready():
-	radius = Vector2(spawn_radius, 0)
-	
-	spawn_timer = Timer.new()
-	add_child(spawn_timer)
-	
-	spawn_timer.wait_time = spawn_interval
-	spawn_timer.connect("timeout", _on_spawn_timer_timeout)
-	spawn_timer.start()
+var _spawn_timer: Timer
 
-func configure_spawner(spawner_config: SpawnerConfig):
-	pass
+
+func _ready():
+	# connect() is getting the "timeout" signal and calling a function when the signal is emitted
+	_spawn_timer = Timer.new()
+	_spawn_timer.connect("timeout", _on_spawn_timer_timeout)
+	add_child(_spawn_timer)
+	
+	# default config defined manually
+	configure_spawner(spawn_radius, spawn_interval)
+	
+	start_spawner()
+
+
+func start_spawner():	
+	_spawn_timer.start()
+
+
+func stop_spawner():
+	_spawn_timer.stop()
+
+
+func configure_spawner(radius: float, interval: float):
+	_spawn_timer.set_wait_time(interval)
+	print(interval)
+
 
 func _on_spawn_timer_timeout():
 	const enemy_scene = preload("res://Scenes/bear_test.tscn")
@@ -25,9 +38,12 @@ func _on_spawn_timer_timeout():
 	
 	var center_coordinates: Vector2 = get_viewport_rect().size / 2
 	var rng = RandomNumberGenerator.new()
-	var random_angle = rng.randf_range(0, 360.0)
-	var spawn_position: Vector2 = center_coordinates + radius.rotated(random_angle * (2 * PI))
+	var random_angle = rng.randf_range(-180, 180)
+	
+	var rotated = Vector2(spawn_radius, 0).rotated(random_angle * (2 * PI))
+	
+	var spawn_position: Vector2 = center_coordinates + rotated
 	
 	enemy.transform.origin = spawn_position
-
+	
 	add_child(enemy)
