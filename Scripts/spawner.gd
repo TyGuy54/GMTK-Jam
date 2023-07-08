8ee3700a6f6351d7c79cbf8311840b3eb75efd53
zanecosmo <zanecosmo@gmail.com>
2023-07-08 12:14:46 -0400

    spawner-test

--- Scripts/spawner.gd
+++ Scripts/spawner.gd
@@ -1,21 +1,30 @@
 extends Node2D
 
-var spawnTimer: Timer
+var spawn_timer: Timer
 var vector: Vector2
+@export var spawn_radius: Vector2
 	
 func _ready():
-	spawnTimer = Timer.new()
-	add_child(spawnTimer)
-	spawnTimer.wait_time = 1.0
-	spawnTimer.connect("timeout", _on_spawn_timer_timeout)
-	spawnTimer.start()
+	spawn_radius = Vector2(700, 0)
+	spawn_timer = Timer.new()
+	add_child(spawn_timer)
+	spawn_timer.wait_time = 1.0
+	spawn_timer.connect("timeout", _on_spawn_timer_timeout)
+	spawn_timer.start()
 
-var counter: int = 0;
+var counter: int = 0
 
 func _on_spawn_timer_timeout():
 	const enemy_scene = preload("res://Scenes/bear_test.tscn")
 	var enemy = enemy_scene.instantiate();
-	enemy.transform.origin = Vector2(counter, 100)
-	add_child(enemy)
-	counter+= 100;
 	
+	var center_coordinates = get_viewport_rect().size / 2
+	
+	var rng = RandomNumberGenerator.new()
+	var my_random_number = rng.randf_range(0, 360.0)
+
+	var spawn_position: Vector2 = center_coordinates + spawn_radius.rotated(10)
+	
+	enemy.transform.origin = spawn_position
+	add_child(enemy)
+	counter+= 100
